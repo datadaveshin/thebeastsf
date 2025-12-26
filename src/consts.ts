ece3027d0d4ada08247e1598ec3c16aab73d756b
David Shin <davidshin444@gmail.com>
2025-12-25 21:07:46 -0700

    Update main parts
    - Update Index - add categories
    - Switch copyright in footer
    - Update title and other constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'The Beast SF';
+export const SITE_DESCRIPTION = 'The Beast is coming to San Franciso! This site is to help The Beast plan his visit.';
